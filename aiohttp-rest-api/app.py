5a27d546fcde57c9daff66f37c7d037113cbde7e
julianapsilva <julianadepaula001@gmail.com>
2022-03-02 10:55:06 -0300

    returning json from the getAll method

--- aiohttp-rest-api/app.py
+++ aiohttp-rest-api/app.py
@@ -2,7 +2,6 @@ from sqlalchemy import insert, select
 from aiohttp import web
 from db.data_definition import Materia
 from db.data_connection import engine
-import json
 
 
 async def add_class(request: web.Request) -> web.Response:
@@ -16,14 +15,12 @@ async def add_class(request: web.Request) -> web.Response:
 
 
 async def get_all_classes(request: web.Request) -> web.Response:
-    stmt = Materia.select().select_from(Materia)
+    stmt = Materia.select().select_from(Materia).order_by('id')
     with engine.connect() as conn:
-        qs =  conn.execute(stmt)
-        teste = {r for r in qs.fetchall()}
-        print(teste)
-    return web.json_response(teste)
-
-
+        qs = conn.execute(stmt)
+        header = ('id', 'name', 'cargaHoraria', 'description')
+        result = [dict(zip(header, r)) for r in qs.fetchall()]
+    return web.json_response(result)
 
 
 async def init_app() -> web.Application:
